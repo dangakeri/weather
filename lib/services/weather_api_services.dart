4bbdf9cc1604426936bef67c4659dd49beac3992
dangakeri <dangakeri254@gmail.com>
2023-05-18 09:48:44 +0300

    weather api service

--- lib/services/weather_api_services.dart
+++ lib/services/weather_api_services.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:weather/consts/constants.dart';
 import 'package:weather/consts/http_error_handler.dart';
+import 'package:weather/exceptions/weather_exceptions.dart';
+import 'package:weather/models/weather.dart';
 
 class WeatherApiServices {
   final http.Client httpClient;
@@ -23,8 +25,36 @@ class WeatherApiServices {
         throw httpErrorHandler(response);
       }
       final responseBody = json.decode(response.body);
+      if (responseBody.isEmpty) {
+        throw WeatherException('Cannot get the woeid of the$city');
+      }
+      if (responseBody.length > 1) {
+        throw WeatherException(
+            'There are multiple candidates for city, please specify furthur');
+      }
+      return responseBody[0]['woeid'];
     } catch (e) {
-      print(e);
+      rethrow;
+    }
+  }
+
+  Future<Weather> getWeather(int woeid) async {
+    final Uri uri = Uri(
+      scheme: 'https',
+      host: kHost,
+      path: '/api/location/$woeid',
+    );
+    try {
+      final http.Response response = await http.get(uri);
+      if (response.statusCode != 200) {
+        throw Exception(httpErrorHandler(response));
+      }
+      final weatherJson = json.decode(response.body);
+      final Weather weather = Weather.fromJson(weatherJson);
+      print(weather);
+      return weather;
+    } catch (e) {
+      rethrow;
     }
   }
 }
