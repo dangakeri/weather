5be5012b21f202561002c6a6f570df424e5c642a
dangakeri <dangakeri254@gmail.com>
2023-05-18 10:11:50 +0300

    weather repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
-
+import 'package:weather/repositories/weather_repository.dart';
+import 'package:weather/services/weather_api_services.dart';
+import 'package:http/http.dart' as http;
 import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
